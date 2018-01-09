27002ad76218da635cf54935340e49de99b18eb7
CarlsenEric <ecc@rincon.com>
2018-01-09 11:16:18 -0700

    Begin transitioning to c structure. Remove cython generated C file.

--- cython_nbody2.pyx
+++ cython_nbody2.pyx
@@ -24,23 +24,54 @@ SOLAR_MASS = 4 * PI * PI
 DAYS_PER_YEAR = 365.24
 
 # TODO Begin transitioning to using C structure to hold body information
-# cdef struct c_body:
-#     double x[3]
-#     double v[3]
-#     double mass
-# 
-# cdef c_body c_bodies[5]
-# c_bodies[0].x = [0.0, 0.0, 0.0]
-# c_bodies[0].v = [0.0, 0.0, 0.0]
-# c_bodies[0].mass = SOLAR_MASS
-# 
-# c_bodies[1].x = [4.84143144246472090e+00,
-#                  -1.16032004402742839e+00,
-#                  -1.03622044471123109e-01]
-# c_bodies[1].v = [1.66007664274403694e-03 * DAYS_PER_YEAR,
-#                  7.69901118419740425e-03 * DAYS_PER_YEAR,
-#                  -6.90460016972063023e-05 * DAYS_PER_YEAR]
-# c_bodies[1].mass = 9.54791938424326609e-04 * SOLAR_MASS
+cdef struct c_body:
+    double x[3]
+    double v[3]
+    double mass
+    double fill
+
+cdef c_body c_bodies[5]
+
+# Sun
+c_bodies[0].x = [0.0, 0.0, 0.0]
+c_bodies[0].v = [0.0, 0.0, 0.0]
+c_bodies[0].mass = SOLAR_MASS
+
+# Jupiter
+c_bodies[1].x =  [ 4.84143144246472090e+00,
+                  -1.16032004402742839e+00,
+                  -1.03622044471123109e-01]
+c_bodies[1].v =  [ 1.66007664274403694e-03 * DAYS_PER_YEAR,
+                   7.69901118419740425e-03 * DAYS_PER_YEAR,
+                  -6.90460016972063023e-05 * DAYS_PER_YEAR]
+c_bodies[1].mass = 9.54791938424326609e-04 * SOLAR_MASS
+
+# Saturn
+c_bodies[2].x =  [ 8.34336671824457987e+00,
+                   4.12479856412430479e+00,
+                  -4.03523417114321381e-01]
+c_bodies[2].v =  [-2.76742510726862411e-03 * DAYS_PER_YEAR,
+                   4.99852801234917238e-03 * DAYS_PER_YEAR,
+                   2.30417297573763929e-05 * DAYS_PER_YEAR]
+c_bodies[2].mass = 2.85885980666130812e-04 * SOLAR_MASS
+
+# Uranus
+c_bodies[3].x =  [ 1.28943695621391310e+01,
+                  -1.51111514016986312e+01,
+                  -2.23307578892655734e-01]
+c_bodies[3].v =  [ 2.96460137564761618e-03 * DAYS_PER_YEAR,
+                   2.37847173959480950e-03 * DAYS_PER_YEAR,
+                  -2.96589568540237556e-05 * DAYS_PER_YEAR]
+c_bodies[3].mass = 4.36624404335156298e-05 * SOLAR_MASS
+
+# Neptune
+c_bodies[4].x =  [ 1.53796971148509165e+01,
+                  -2.59193146099879641e+01,
+                   1.79258772950371181e-01]
+c_bodies[4].v =  [ 2.68067772490389322e-03 * DAYS_PER_YEAR,
+                   1.62824170038242295e-03 * DAYS_PER_YEAR,
+                  -9.51592254519715870e-05 * DAYS_PER_YEAR]
+c_bodies[4].mass = 5.15138902046611451e-05 * SOLAR_MASS 
 
 BODIES = {
     'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
@@ -134,6 +165,7 @@ def main(int n=5000000, ref='sun'):
     report_energy()
     advance(0.01, n)
     report_energy()
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1: 
