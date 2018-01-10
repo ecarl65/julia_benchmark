c43803c21711823f327af2dea7ccae4e88b3dd54
Eric Carlsen <ecarl65@gmail.com>
2018-01-09 22:43:44 -0700

    Make setup.py include the needed compile args

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("cython_nbody2.pyx")
+#    ext_modules = cythonize("cython_nbody2.pyx")
+    ext_modules = cythonize([Extension(name="cython_nbody2", sources=["cython_nbody2.pyx"], language='c', extra_compile_args=["-Ofast", "-march=native"])])
 )
