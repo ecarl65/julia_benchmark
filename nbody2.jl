ad9d1c1482811a33226c7a6a775c96d9644b7a90
Eric Carlsen <ecarl65@gmail.com>
2018-10-06 08:14:10 -0600

    Pass array in as argument, use in-place operations

--- nbody2.jl
+++ nbody2.jl
@@ -18,7 +18,7 @@ mutable struct Body
 end
 
 # Define each of the body initial values
-bodies = SVector(
+_bodies = SVector(
                  # Sun
                  Body(SVector(0.0, 0.0, 0.0), 
                       0.0, 
@@ -63,12 +63,12 @@ bodies = SVector(
                 )
 
 # Pre-define some arrays and values globally to reduce the amount of copying
-dx = zeros(3)
+const dx = zeros(3)
 
 """
 Not sure exactly what this is supposed to be doing
 """
-function offset_momentum()
+function offset_momentum(bodies)
     for i = 1:length(bodies)
         for k = 1:3
             bodies[1].v[k] -= bodies[i].v[k] * bodies[i].mass / SOLAR_MASS
@@ -79,22 +79,22 @@ end
 """
 Advance the positions and velocities
 """
-function bodies_advance(dt::Float64)
-    dx = @SVector zeros(3)
+function bodies_advance!(bodies,dt::Float64)
+    fill!(dx, 0.0)
     dsq = 0.
     distance = 0.
     mag = 0.
 
     for i = 1:length(bodies)
         for j = (i+1):length(bodies)
-            dx = bodies[i].x - bodies[j].x
+            @. dx = bodies[i].x - bodies[j].x
 
             dsq = dot(dx, dx)
             distance = sqrt(dsq)
             mag = dt / (dsq * distance)
 
-            bodies[i].v -= dx * bodies[j].mass * mag
-            bodies[j].v += dx * bodies[i].mass * mag
+            @. bodies[i].v -= dx * bodies[j].mass * mag
+            @. bodies[j].v += dx * bodies[i].mass * mag
         end
     end
 
@@ -106,8 +106,8 @@ end
 """
 Compute the overall energy in the system
 """
-function bodies_energy()
-    dx = @SVector zeros(3)
+function bodies_energy(bodies)
+    fill!(dx, 0.0)
     distance = 0.
     energy = 0.
 
@@ -115,7 +115,7 @@ function bodies_energy()
         energy += bodies[i].mass * dot(bodies[i].v, bodies[i].v) / 2.0
 
         for j = (i+1):length(bodies)
-            dx = bodies[i].x - bodies[j].x
+            @. dx = bodies[i].x - bodies[j].x
 
             distance = norm(dx)
             energy -= (bodies[i].mass * bodies[j].mass) / distance
@@ -125,19 +125,19 @@ function bodies_energy()
 end
 
 
-function main_loop(N::Int64)
+function main_loop(N::Int64,bodies)
 
     # Pre-allocate some arrays and variables
 
-    offset_momentum()
+    offset_momentum(bodies)
 
-    @printf "%.9f\n" bodies_energy()
+    @printf "%.9f\n" bodies_energy(bodies)
 
-    (for i = 1:N; bodies_advance(0.01); end)
+    for i = 1:N; bodies_advance!(bodies,0.01); end
 
     #Profile.print()
 
-    @printf "%.9f\n" bodies_energy()
+    @printf "%.9f\n" bodies_energy(bodies)
 
 end
 
@@ -146,5 +146,5 @@ if length(ARGS) >= 1
     N = parse(Int64, ARGS[1])
 end
 
-@time main_loop(N)
-#main_loop(N)
+main_loop(N,_bodies)
+@time main_loop(N,_bodies)
